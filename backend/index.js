06878cec824ee13aa620dfde9c05e4599b8e174e
Pavitrank <pavitrank2002@gmail.com>
2025-09-13 19:18:31 +0530

    "chore: Connect Node.js server with MongoDB"

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,24 @@ console.log("This is index");
 
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
+mongoose.connect(
+  "mongodb+srv://pavitrank2002_db_user:m5xkm8kRYggmSYZG@cluster0.7suw6vi.mongodb.net/"
+);
+
+const User = require("./userModel");
+
+async function insert() {
+  {
+    await User.create({
+      name: "Sandeep",
+      email: "sandeep@gmail.com",
+    });
+  }
+};
+
+insert();
 app.get("/api", (req, res) => {
   res.json({ users: ["userOne", "userTwo", "userThree"] });
 });
