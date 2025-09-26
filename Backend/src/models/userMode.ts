390a00fc501c62ce2d08f64680f2bb32c6668873
Pavitrank <pavitrank2002@gmail.com>
2025-09-26 13:18:52 +0530

    "feat: Create user schema"

---
+++ Backend/src/models/userMode.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+// Schema
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: [true, "User name is required"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+    phone: {
+        type: String,
+        required: [true, "Phone number is required"]
+    }
+});
+
+export default mongoose.model("User", userSchema);
