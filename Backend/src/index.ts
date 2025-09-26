85abdf71a31c8d868acd5a3a0deed6db4a84f40f
Pavitrank <pavitrank2002@gmail.com>
2025-09-26 13:19:13 +0530

    "fix: Updated port number"

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -16,7 +16,7 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
