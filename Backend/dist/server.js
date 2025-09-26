7c88d44600470ee50ab76ead0b730af46143ae24
Pavitrank <pavitrank2002@gmail.com>
2025-09-26 08:19:17 +0530

    "chore: "Resolved .ts not found error"

---
+++ Backend/dist/server.js
@@ -0,0 +1,7 @@
+import express from "express";
+const app = express();
+app.get("/", (req, res) => {
+    res.send("Server is running smoothly");
+});
+export default app;
+//# sourceMappingURL=server.js.map
