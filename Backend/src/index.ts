ec2465b8992bd1a5be858c218e031f7942ce9dba
Pavitrank <pavitrank2002@gmail.com>
2025-09-26 17:02:05 +0530

    "chore: Removed port variable"

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -22,7 +22,9 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
+console.log(PORT);
+console.log("PORT is: ", process.env.port);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
