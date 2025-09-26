1b31ba60b470e2bc0ba3eb667bc84ad2754aa247
Pavitrank <pavitrank2002@gmail.com>
2025-09-26 12:50:20 +0530

    "feat: Setup basic server"

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,5 +1,22 @@
-import app from "./server.ts";
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import connectDB from "./config/db.ts";
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+dotenv.config();
+
+// Connect to DB
+connectDB();
+
+const app = express();
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("Server is running smoothly");
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
--- Backend/src/server.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const app = express();
-
-app.get("/", (req, res) => {
-    res.send("Server is running smoothly");
-});
-
-export default app;
