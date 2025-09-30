14315ef1c96f458adf0ed67798255a2a24fe582c
Pavitrank <pavitrank2002@gmail.com>
2025-09-30 18:30:17 +0530

    "style: Add space at top"

--- Frontend/travel-buddy/src/screens/SigninScreen.tsx
+++ Frontend/travel-buddy/src/screens/SigninScreen.tsx
@@ -17,15 +17,14 @@ const SigninScreen = ({navigation}) => {
 
                 <View className='w-[80%] flex gap-y-10 items-center justify-center'>
                     <Text className='text-white text-3xl border-b-4 border-green-600 w-full  rounded-2xl text-center pb-2'>LOGIN</Text>
-
-                    <TextInput placeholder="Enter your email" className='border border-gray-400 w-full px-2 rounded-lg text-white h-14 tracking-widest text-xl' placeholderTextColor="#999"></TextInput>
-                    <TextInput placeholder="Enter your Password" className='border border-gray-400 w-full px-2 rounded-lg text-white h-14 tracking-widest text-xl' placeholderTextColor="#999"></TextInput>
+                    <TextInput placeholder="Enter your email" className='border border-gray-400 w-full px-2 rounded-lg text-white h-14 tracking-widest text-2xl' placeholderTextColor="#999"></TextInput>
+                    <TextInput placeholder="Enter your Password" className='border border-gray-400 w-full px-2 rounded-lg text-white h-14 tracking-widest text-2xl' placeholderTextColor="#999"></TextInput>
                     <View className='w-full flex items-center gap-y-6'>
                         <TouchableOpacity className={`w-full py-4 rounded-xl items-center ${pressed ? "bg-green-500" : "bg-green-600"}`} onPressIn={() => setPressed(true)} onPressOut={() => setPressed(false)} activeOpacity={0.8}>
                             <Text className='text-white text-2xl font-medium tracking-wider'>Submit</Text>
                         </TouchableOpacity>
                         <View>
-                            <Text className='text-white tracking-wider text-xl'>New User ? <Text className='text-green-600' onPress={() => handleNavigation()}>Signup</Text></Text>
+                            <Text className='text-white tracking-wider text-xl'>New User ? <Text className='text-green-600 tracking-widest' onPress={() => handleNavigation()}>SignUp</Text></Text>
                         </View>
                     </View>
                 </View>
--- Frontend/travel-buddy/src/screens/SignupScreen.tsx
+++ Frontend/travel-buddy/src/screens/SignupScreen.tsx
@@ -1,10 +1,30 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
 
-const SignupScreen = () => {
+const SignupScreen = ({ navigation }) => {
+  const [pressed, setPressed] = useState(false);
+  function handleNavigation() {
+    navigation.navigate("SignIn");
+  }
   return (
-    <View>
-      <Text>SignupScreen</Text>
+    <View className='h-full w-full -bg--color-dark--1 flex items-center pt-12'>
+      <View className='flex justify-center items-center gap-y-10 py-12 pt-20'>
+        <Image className='h-[100] w-[105] ' source={require('../assets/Logo.png')}></Image>
+        <Text className='text-white text-3xl text-semibold tracking-widest'>Travel. Note. Remember</Text>
+      </View>
+      <View className='w-[80%] flex gap-y-10 items-center justify-center'>
+        <Text className='text-white text-3xl border-b-4 border-green-600 w-full text-center rounded-2xl pb-2'>SIGNUP</Text>
+        <TextInput placeholder="Enter your email" className='border border-gray-400 w-full px-2 rounded-lg text-white h-14 tracking-widest text-2xl' placeholderTextColor="#999"></TextInput>
+        <TextInput placeholder="Enter your Password" className='border border-gray-400 w-full px-2 rounded-lg text-white h-14 tracking-widest text-2xl' placeholderTextColor="#999"></TextInput>
+        <View className='w-full flex items-center gap-y-6'>
+          <TouchableOpacity className={`w-full py-4 rounded-xl items-center ${pressed ? "bg-green-500" : "bg-green-600"}`} onPressIn={() => setPressed(true)} onPressOut={() => setPressed(false)} activeOpacity={0.8}>
+            <Text className='text-white text-2xl font-medium tracking-wider'>Submit</Text>
+          </TouchableOpacity>
+          <View>
+            <Text className='text-white tracking-wider text-xl'>Alreay a User ? <Text className='text-green-600 tracking-widest' onPress={() => handleNavigation()}>SignIn</Text></Text>
+          </View>
+        </View>
+      </View>
     </View>
   )
 }
