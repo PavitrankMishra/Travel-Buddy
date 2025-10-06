af0f1543a8e55a8d2a374ea1b41858be476f0f9c
Pavitrank <pavitrank2002@gmail.com>
2025-10-06 23:36:29 +0530

    "feat: User registration using redux store"

--- Frontend/travel-buddy/src/screens/SignupScreen.tsx
+++ Frontend/travel-buddy/src/screens/SignupScreen.tsx
@@ -1,76 +1,82 @@
 import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Spinner from '../components/Spinner';
+import { useDispatch, useSelector } from 'react-redux';
+import { registerUser, resetState } from '../store/slice/UserRegistrationSlice';
 
 const SignupScreen = ({ navigation }) => {
-  const [pressed, setPressed] = useState(false);
-  const [userName, setUserName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [phone, setPhone] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [customMessage, setCustomMessage] = useState("");
+  // State the toggles if button is pressed
+  const [pressed, setPressed] = useState<boolean>(false);
+
+  // Stores the userName of the user
+  const [userName, setUserName] = useState<string>("");
+
+  // Stores the email of the user
+  const [email, setEmail] = useState<string>("");
+
+  // Stores the password of the user
+  const [password, setPassword] = useState<string>("");
+
+  // Stores the phone number of the user
+  const [phone, setPhone] = useState<string>("");
+
+  // Function that navigates to SignIn page
   function handleNavigation() {
     navigation.navigate("SignIn");
   }
 
+  // Function that updates the userName state
   function handleUserNameInput(text) {
     setUserName(text);
   }
+
+  // Function that updates the email state
   function handleEmailInput(text) {
     setEmail(text)
   }
 
+  // Function that updates the password state
   function handlePasswordInput(text) {
     setPassword(text);
   }
 
+  // Function that updates the phone state
   function handlePhoneInput(text) {
     setPhone(text);
   }
 
-  async function handleSubmit() {
-    try {
-      setLoading(true);
-      if (userName.length === 0 || email.length === 0 || password.length === 0 || phone.length === 0) {
-        setLoading(false);
-        throw new Error("All fields are required");
-      }
-      const res = await fetch("https://travel-buddy-r69f.onrender.com/api/v1/auth/register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          "userName": userName,
-          "email": email,
-          "password": password,
-          "phone": phone
-        })
-      });
+  const dispatch = useDispatch();
 
-      const data = await res.json();
-      if (data.success === true) {
-        setTimeout(() => {
-          navigation.navigate("SignIn");
-          setCustomMessage("");
-        }, 5000);
-        setTimeout(() => {
-          setLoading(false);
-          setCustomMessage("User Registered successfully");
-        }, 2000);
-      } else {
-        throw new Error(data.message);
-      }
-    } catch (err) {
-      setLoading(false);
-      setCustomMessage(err.message);
-      setTimeout(() => {
-        setCustomMessage("");
-      }, 2000);
-      console.log("The error is: ", err);
+  const registerSuccess = useSelector((state: any) => state.userRegistration.success);
+  const registerLoading = useSelector((state: any) => state.userRegistration.loading);
+  const registerError = useSelector((state: any) => state.userRegistration.error);
+  const registerMessage = useSelector((state: any) => state.userRegistration.message);
+
+  // Function that handles user registration process
+  // Function that does a POST request to register user
+  // Throws error if either of userName, email, password and phone are empty
+  async function handleSubmit() {
+    const registerData = {
+      userName: userName,
+      email: email,
+      password: password,
+      phone: phone,
     }
+    dispatch(registerUser(registerData));
   }
+
+  useEffect(() => {
+    if (registerError === true) {
+      setTimeout(() => {
+        dispatch(resetState());
+      }, 2000);
+    } else if(registerSuccess === true) {
+      setTimeout(() => {
+        navigation.navigate("SignIn");
+        dispatch(resetState());
+      }, 2000);
+    }
+  }, [registerSuccess, registerError]);
 
   return (
     <View className='h-full w-full -bg--color-dark--1 flex items-center pt-20'>
@@ -86,12 +92,13 @@ const SignupScreen = ({ navigation }) => {
         <TextInput placeholder="Phone " className='border border-gray-400 w-full px-2 rounded-lg text-white h-14 tracking-widest text-xl' placeholderTextColor="#999" value={phone} onChangeText={(text) => setPhone(text)}></TextInput>
         <View className='w-full flex items-center gap-y-6'>
           <TouchableOpacity className={`w-full py-3 rounded-xl items-center ${pressed ? "bg-green-500" : "bg-green-600"}`} onPressIn={() => setPressed(true)} onPressOut={() => setPressed(false)} activeOpacity={0.8} onPress={() => handleSubmit()}>
-            {loading ? (<Text><Spinner /></Text>) : (<Text className='text-white text-2xl font-medium tracking-wider'>Submit</Text>)}
+            {registerLoading ? (<Text><Spinner /></Text>) : (<Text className='text-white text-2xl font-medium tracking-wider'>Submit</Text>)}
           </TouchableOpacity>
           <View>
             <Text className='text-white tracking-wider text-xl'>Alreay a User ? <Text className='text-green-600 tracking-widest' onPress={() => handleNavigation()}>SignIn</Text></Text>
           </View>
-          {customMessage ? (<View><Text className='text-white text-xl'>{customMessage}</Text></View>) : (<Text></Text>)}
+          {registerError === true && (<View><Text className='text-white text-xl tracking-widest'>{registerMessage}</Text></View>)}
+          {registerSuccess === true && (<View><Text className='text-white text-xl tracking-widest'>{registerMessage}</Text></View>)}
         </View>
       </View>
     </View>
