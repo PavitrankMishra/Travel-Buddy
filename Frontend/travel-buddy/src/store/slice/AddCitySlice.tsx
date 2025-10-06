4e0a75ce8dea490335c358097a4c3eee5e10b913
Pavitrank <pavitrank2002@gmail.com>
2025-10-06 23:38:09 +0530

    "feat: Slice to add new city"

---
+++ Frontend/travel-buddy/src/store/slice/AddCitySlice.tsx
@@ -0,0 +1,78 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+interface AddCityState {
+    loading: boolean;
+    success: boolean;
+    successMessage: string;
+    error: boolean;
+    errorMessage: string;
+}
+
+const initialState: AddCityState = {
+    loading: false,
+    success: false,
+    successMessage: "",
+    error: false,
+    errorMessage: ""
+}
+
+export const addUserCities = createAsyncThunk("submitCityForm", async (sentData, { rejectWithValue }) => {
+    try {
+        const res = await fetch(
+            "https://travel-buddy-r69f.onrender.com/api/v1/cities/addCity",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(sentData),
+            }
+        );
+
+        const data = await res.json();
+
+        if (data.success === false) {
+            return rejectWithValue(data.message || "Failed to add city")
+        } else {
+            return data;
+        }
+
+
+    } catch (err: any) {
+        return rejectWithValue(err.message || "Something went wrong");
+    }
+});
+
+const addCitySlice = createSlice({
+    name: 'addCity',
+    initialState,
+    reducers: {
+        resetState: (state) => {
+            state.error = false;
+            state.success = false;
+            state.errorMessage = "";
+            state.successMessage = "";
+            state.loading = false;
+        }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(addUserCities.pending, (state) => {
+            state.loading = true;
+            state.success = false;
+            state.error = false;
+        })
+            .addCase(addUserCities.fulfilled, (state, action) => {
+                state.loading = false;
+                state.success = true;
+                state.successMessage = action.payload.message || "Success";
+            })
+            .addCase(addUserCities.rejected, (state, action) => {
+                state.loading = false;
+                state.error = true;
+                state.errorMessage = action.payload as string;
+            })
+    }
+});
+
+export const { resetState } = addCitySlice.actions;
+export default addCitySlice.reducer;
