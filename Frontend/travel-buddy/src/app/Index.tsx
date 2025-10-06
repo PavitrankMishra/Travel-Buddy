dca366fd8141f39d4270126a58a2aef11b9e374a
Pavitrank <pavitrank2002@gmail.com>
2025-10-06 23:35:18 +0530

    "chore: Wrap app with Redux provider to enable global state mangement"

--- Frontend/travel-buddy/src/app/Index.tsx
+++ Frontend/travel-buddy/src/app/Index.tsx
@@ -5,15 +5,19 @@ import SigninScreen from '../screens/SigninScreen';
 import SignupScreen from '../screens/SignupScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
+import { Provider } from 'react-redux';
+import store from '../store/store';
 
 const Stack = createNativeStackNavigator();
 const Index = () => {
     return (
-        <Stack.Navigator initialRouteName='SignIn'>
-            <Stack.Screen name='SignIn' component={SigninScreen} options={{ headerShown: false }} />
-            <Stack.Screen name="SignUp" component={SignupScreen} options={{ headerShown: false }} />
-            <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-        </Stack.Navigator>
+        <Provider store={store}>
+            <Stack.Navigator initialRouteName='SignIn'>
+                <Stack.Screen name='SignIn' component={SigninScreen} options={{ headerShown: false }} />
+                <Stack.Screen name="SignUp" component={SignupScreen} options={{ headerShown: false }} />
+                <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+            </Stack.Navigator>
+        </Provider>
     )
 }
 
