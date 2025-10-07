b2c44d7e1c665afd020ea6dcb28fab81b6741516
Pavitrank <pavitrank2002@gmail.com>
2025-10-07 22:49:50 +0530

    "feat: Add userLogin and deleteCity reducer"

--- Frontend/travel-buddy/src/store/store.tsx
+++ Frontend/travel-buddy/src/store/store.tsx
@@ -2,12 +2,16 @@ import { configureStore } from '@reduxjs/toolkit';
 import addCityReducer from "../store/slice/AddCitySlice";
 import userCityReducer from "../store/slice/UserCitySlice";
 import userRegistrationReducer from "../store/slice/UserRegistrationSlice";
+import userLoginReducer from "../store/slice/UserLoginSlice";
+import deleteCityReducer from "../store/slice/DetailsDeleteSlice";
 
 export const store = configureStore({
     reducer: {
         addCity: addCityReducer,
         userCity: userCityReducer,
         userRegistration: userRegistrationReducer,
+        userLogin: userLoginReducer,
+        deleteCity: deleteCityReducer,
     },
 });
 
